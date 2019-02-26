926f9a10a4e497baa156b3caf0454492fa7e05df
vscopise <vscopise@gmail.com>
2019-02-26 18:57:53 -0300

    26/2 1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,100 @@
 import React, { Component } from 'react';
 import * as Constants from './assets/Constants';
 
-import {Carousel} from 'react-responsive-carousel'
-import 'react-responsive-carousel/lib/styles/carousel.min.css'
 
-import logo from './logo.svg';
+import Slider from 'react-animated-slider';
+import 'react-animated-slider/build/horizontal.css';
+
 import './App.css';
+import './assets/animations.css';
 
-const Loader = () =>(
+const Loader = () => (
   <div className='loader' />
 )
+
+class SliderProducts extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      token: this.props.token,
+      categories: this.props.categories,
+      products: false,
+    }
+  }
+
+  componentDidMount() {
+    this.fetchProducts();
+  }
+
+  fetchProducts = () => {
+    var category = '';
+    var location = window.location.pathname.split('/').filter(x=>x).pop();
+    if ( '' !== window.location.pathname ) {
+      var category = '?category=' + this.state.categories.find(
+        cat => cat.slug === location
+      ).id
+    }
+    fetch(Constants.apiUrl + 'wc/v3/products/' + category, {
+      headers:{
+          'Content-Type': 'application/json',
+          'accept': 'application/json',
+          'Authorization': 'Bearer ' + this.state.token
+      },
+    })
+    .then(response => response.json())
+    .then((products) => this.setState({
+        isLoading: false,
+        products
+    }))
+  }
+
+
+  render() {
+    return(
+      <div>
+      {
+        !this.state.isLoading && 
+        <Slider
+          autoplay={3000}
+          //disabled={true}
+          className='slider-wrapper'
+        >
+        {
+            this.state.products.map( product => (
+                <div
+                  key={product.id}
+                  className='slider-content'
+                  style={{ background: `url('${product.images[0].src}') center left no-repeat` }}
+                >
+                    <div className="inner">
+                      <h2>{product.name}</h2>
+                      <div 
+                        className='extracto'
+                        dangerouslySetInnerHTML={{__html: product.description}}
+                      />
+                      {
+                        product.regular_price !== '' &&
+                        <p 	className="precio-anterior">{product.regular_price}</p>
+                      }
+                      {
+                        product.sale_price !== '' &&
+                        <p 	className="precio-nuevo">{product.sale_price}</p>
+                      }
+                      <p 	className="precio_vigencia"> 30/03</p>
+                    </div> 
+                </div>
+            ))
+        }
+        </Slider>
+      }
+      {
+        this.state.isLoading && <Loader />
+      }
+      </div>
+    )
+  }
+}
 
 class App extends Component {
   constructor(props) {
@@ -20,6 +105,7 @@ class App extends Component {
     }
   }
   componentDidMount() {
+    //console.log(window.location.search);
     this.fetchToken();
   }
 
@@ -39,68 +125,39 @@ class App extends Component {
         token: data.token,
         isLoading: false,
     }))
-    .then( this.fetchProducts )
+    .then( this.fetchCategories )
+    
     .catch(error => console.log(error))
   }
 
-  fetchProducts = () => {
-    fetch(Constants.apiUrl + 'wc/v3/products/', {
+  fetchCategories = () => {
+    fetch(Constants.apiUrl + 'wc/v3/products/categories?per_page=20', {
       headers:{
           'Content-Type': 'application/json',
           'accept': 'application/json',
           'Authorization': 'Bearer ' + this.state.token
       },
-  })
-  .then(response => response.json())
-  .then((products) => this.setState({
-      isLoading: false,
-      products
-  }))
+    })
+    .then(response => response.json())
+    .then((categories) => this.setState({
+        categories
+    }))
+    //.then( this.fetchProducts )
   }
 
+  
   render() {
     return (
       <div className="App">
       {
-        this.state.products &&
-        <Carousel
-            showArrows={false}
-            showStatus={false}
-            showIndicators={false}
-            showThumbs={false}
-            autoPlay={true}
-            infiniteLoop={true}
-            interval={6000}
-            transitionTime={1200}
-            dynamicHeight={true}
-        >
-        {
-            this.state.products.map( product => (
-                <div key={product.id}>
-                  <img src={product.images[0].src} alt={product.title} />
-                    <div className="slide-content">
-                      <p 	className="nombre">{product.title}</p>
-                      <div 
-                        className='extracto'
-                        dangerouslySetInnerHTML={{__html: product.description}}
-                      />
-                      {
-                        product.regular_price !== '' &&
-                        <p 	className="precio-anterior">{product.regular_price}</p>
-                      }
-                      {
-                        product.sale_price !== '' &&
-                        <p 	className="precio-nuevo">{product.sale_price}</p>
-                      }
-                      <p 	className="precio_vigencia"> 30/03</p>
-                    </div> 
-                </div>
-            ))
-        }
-        </Carousel>
+        this.state.categories &&
+        <SliderProducts
+          token={this.state.token}
+          categories={this.state.categories}
+        />
       }
       {
-        !this.state.products && <Loader/>
+        !this.state.categories && <Loader/>
       }
       </div>
     );
